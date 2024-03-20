47a56b08d39395b9db4970e16cf09a56addd60bf
Igor Cechinato de Lima <156817223+IgorCdeLima@users.noreply.github.com>
2024-03-20 17:57:08 -0300

    version _6

--- javascript/JavaScript1.js
+++ javascript/JavaScript1.js
@@ -3,7 +3,7 @@ function Enviar() {
     var nome = document.getElementById("nomeid");
 
     if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+        alert('Obrigado sr(a) ' + nome.value + 'contudo essa funcionalidade não está ativa no momento. Envie uma mensagem para Igor.c.l.oficial@outlook.com');
     }
 
 }
